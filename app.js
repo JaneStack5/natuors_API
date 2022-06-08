11c172e0440699cc338e3a16ceabfa5ae5640e72
JaneStack <mrumunstack@gmail.com>
2022-06-08 12:30:15 -0700

    Handling Patch request.

--- app.js
+++ app.js
@@ -55,6 +55,21 @@ app.get('/api/v1/tours/:id', (req, res) => {
     })
 })
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour: 'Update tour here...'
+        }
+    })
+})
+
 const port = 3000
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
