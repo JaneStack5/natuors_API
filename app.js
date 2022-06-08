61aef473594e80e783860cae69646172cb3a69a4
JaneStack <mrumunstack@gmail.com>
2022-06-08 14:31:27 -0700

    User route implemented

--- app.js
+++ app.js
@@ -100,6 +100,41 @@ const deleteTour = (req, res) => {
     })
 }
 
+const getAllUsers = (req, res,) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const getUser = (req, res,) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const createUser = (req, res,) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const updateUser = (req, res,) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
+const deleteUser = (req, res,) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+}
+
 // Routes
 app
     .route('/api/v1/tours')
@@ -108,8 +143,21 @@ app
 
 app
     .route('/api/v1/tours/:id')
-    .get(getTour).patch(updateTour)
+    .get(getTour)
+    .patch(updateTour)
     .delete(deleteTour)
+
+app
+    .route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser)
+
+app
+    .route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser)
+
 
 // Start Server
 const port = 3000
