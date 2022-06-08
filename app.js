1fb798d50bec2b0bd5a8c470df0c950bd0266f85
JaneStack <mrumunstack@gmail.com>
2022-06-08 05:36:28 -0700

    Handling post request

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const fs = require('fs')
 const express = require('express');
 
-
 const app = express()
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: 'Hello from the server side!'});
-// })
-//
-// app.post('/', (req, res) => {
-//     res.status(200).json({message: 'You can post to this endpoint'});
-// })
+app.use(express.json())
 
 const tours = JSON.parse(
    fs.readFileSync(`${__dirname}/dev-data//data/tours-simple.json`)
@@ -22,6 +15,21 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data//data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
