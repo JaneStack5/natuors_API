19fb1a7de0408b59bc1122dceee2689ad4b209c4
JaneStack <mrumunstack@gmail.com>
2022-06-08 16:47:05 -0700

    A better file structure

---
+++ routes/tourRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+    getTour,
+    getAllTours,
+    createTour,
+    updateTour,
+    deleteTour
+} = require("../controllers/tourControllers")
+const router = express.Router();
+
+router
+    .route('/')
+    .get(getAllTours)
+    .post(createTour)
+
+router
+    .route('/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour)
+
+module.exports = router;
