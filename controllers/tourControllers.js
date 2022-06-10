386a8ff2aaf0b804fde743123e5fc26945e8bb08
JaneStack <mrumunstack@gmail.com>
2022-06-09 22:21:52 -0700

    document update

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -56,13 +56,25 @@ exports.getTour = async (req, res) => {
     }
 }
 
-exports.updateTour =  (req, res) => {
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            tour: 'Update tour here...'
-        }
-    })
+exports.updateTour = async(req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                tour
+            }
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+
 }
 
 exports.deleteTour = (req, res) => {
