665d7b7025a093cc8d7f49980147693ea247c117
JaneStack <mrumunstack@gmail.com>
2022-06-09 23:16:46 -0700

    Tour model completed

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -32,7 +32,7 @@ exports.createTour = async (req, res) => {
     } catch (err) {
       res.status(400).json({
             status: 'fail',
-            message: 'Invalid data sent'
+            message: err
         })
     }
 
@@ -51,7 +51,7 @@ exports.getTour = async (req, res) => {
     } catch(err) {
         res.status(404).json({
             status: 'fail',
-            message: 'Invalid data sent'
+            message: err
         })
     }
 }
