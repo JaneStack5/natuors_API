ce85ad6d622f97bd9d643e35ab33c0a7e81dab0b
JaneStack <mrumunstack@gmail.com>
2022-06-11 07:07:56 -0700

    Making API better with filtering

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -2,8 +2,22 @@ const Tour = require('./../models/Tour')
 
 exports.getAllTours = async (req, res) => {
     try{
-        const tours = await Tour.find()
+        //BUILD QUERY
+        const queryObj = { ...req.query }
+        const excludedFields = ['page', 'sort', 'limit', 'fields']
+        excludedFields.forEach(el => delete queryObj[el])
 
+        const query = Tour.find(queryObj)
+
+        // const query = Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy');
+        //EXECUTE
+        const tours = await query;
+
+        //SEND RESPONSE
         res.status(200).json({
             status: 'success',
             count: tours.length,
