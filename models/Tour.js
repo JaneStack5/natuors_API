86b440d2c336b690154852bf58a9b8d6c3ae22ac
JaneStack <mrumunstack@gmail.com>
2022-06-12 17:19:01 -0700

    Query middlewares

--- models/Tour.js
+++ models/Tour.js
@@ -51,7 +51,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 },{
     toJSON: { virtuals: true },
     toObect: { virtuals: true}
@@ -71,5 +75,19 @@ tourSchema.pre('save', function(next) {
 //     console.log(doc);
 //     next();
 // })
+
+//Query Middleware
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+    this.find({ secretTour: { $ne: true}})
+
+    this.start = Date.now();
+    next()
+})
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+    console.log(docs);
+    next();
+})
 
 module.exports = mongoose.model('Tour', tourSchema);
