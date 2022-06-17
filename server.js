6c41b57b971b7c5362c3155de40aa81f3881c493
JaneStack <mrumunstack@gmail.com>
2022-06-17 16:39:00 -0700

    catching uncaught exceptions.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 
+process.on('unCaughtException', err => {
+    console.log('Uncaught Exception! Shutting down...')
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 dotenv.config({path: './config.env'})
 const app = require('./app')
 
@@ -16,10 +22,13 @@ const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 });
 
+// Error handling
+
 process.on('unhandledRejection', err => {
-    console.log(err.name, err.message);
     console.log('Unhandled Rejection! Shutting down...')
+    console.log(err.name, err.message);
     server.close(() => {
         process.exit(1);
     })
 });
+
