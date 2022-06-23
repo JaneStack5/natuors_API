f9d62af5f13eb260a93467c77bbd8906d9e1e42f
JaneStack <mrumunstack@gmail.com>
2022-06-23 16:56:59 -0700

    Setting security  HTTP headers.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -9,6 +10,10 @@ const userRouter = require('./routes/userRoute')
 
 const app = express()
 // Global Middlewares
+// Set security HTTP headers
+app.use(helmet())
+
+//Development login
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
@@ -20,12 +25,15 @@ const limiter = rateLimit({
 })
 app.use('/api', limiter);
 
-app.use(express.json())
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }))
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
-    console.log(req.headers)
     next()
 })
 
