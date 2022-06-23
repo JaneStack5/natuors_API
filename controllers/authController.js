04a151ff249b0c2a63607e001b173172582c4ef2
JaneStack <mrumunstack@gmail.com>
2022-06-23 16:26:00 -0700

    Sending JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,25 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
 
+    //remove password from output when created
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httponly: true,
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions)
+    //     expires: new Date(
+    //         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    //     ),
+    //     httponly: true,
+    //     secure: req.secure || req.headers('x-forwarded-proto') === 'https'
+    // });
+
+    // Remove Password From Output
+    user.password = undefined;
+
     res.status(statusCode).json({
         status: 'success',
         token,
