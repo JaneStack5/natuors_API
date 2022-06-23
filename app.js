885ef9a337c2ed9924a0c5e7ab7a6718770b9402
JaneStack <mrumunstack@gmail.com>
2022-06-23 16:41:05 -0700

    Rate limit implemented

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -7,10 +8,17 @@ const tourRouter = require('./routes/tourRoute')
 const userRouter = require('./routes/userRoute')
 
 const app = express()
-//Middlewares
+// Global Middlewares
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+    max: 3,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, Please try again in an hour!'
+})
+app.use('/api', limiter);
 
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
--- controllers/authController.js
+++ controllers/authController.js
@@ -24,13 +24,6 @@ const createSendToken = (user, statusCode, res) => {
     };
     if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
     res.cookie('jwt', token, cookieOptions)
-    //     expires: new Date(
-    //         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    //     ),
-    //     httponly: true,
-    //     secure: req.secure || req.headers('x-forwarded-proto') === 'https'
-    // });
-
     // Remove Password From Output
     user.password = undefined;
 
