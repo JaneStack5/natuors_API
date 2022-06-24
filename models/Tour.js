a5f46957cadb9fd21f8d79d9712ab4de9481aa1a
JaneStack <mrumunstack@gmail.com>
2022-06-24 15:38:58 -0700

    Populating tour guide

--- models/Tour.js
+++ models/Tour.js
@@ -142,6 +142,15 @@ tourSchema.pre(/^find/, function(next) {
     this.start = Date.now();
     next()
 })
+
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    })
+    next()
+})
+
 tourSchema.post(/^find/, function (docs, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds!`);
     next();
