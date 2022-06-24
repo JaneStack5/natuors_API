f1329b6620c5e58417395440961dc298b8c9ecde
JaneStack <mrumunstack@gmail.com>
2022-06-23 17:31:03 -0700

    preventing parameter pollution.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize');
-const xss = require(xss-clean)
+const xss = require('xss-clean')
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -35,6 +36,18 @@ app.use(mongoSanitize());
 
 //Data sanitization against XSS
 app.use(xss());
+
+// prevent parameter pollution
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}));
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`))
