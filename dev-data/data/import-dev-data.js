303caa45fbc74c9f816dd3214aa456c01292d306
JaneStack <mrumunstack@gmail.com>
2022-06-24 13:27:21 -0700

    modeling location(Geospatial data)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -16,7 +16,7 @@ mongoose.connect(DB, {
     .then(()=> console.log(`DB  connection successful!`))
 
 //READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 //IMPORT DATA INTO DATABASE
 const importData = async () => {
--- models/Tour.js
+++ models/Tour.js
@@ -1,88 +1,119 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-
 const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-        trim: true,
-        maxlength: [40, 'A tour name must have less or equal than 40 characters'],
-        minlength: [10, 'A tour name must have more or equal than 10 characters'],
-        // validate: [validator.isAlpha, 'Tour name must only contain characters']
-    },
-    slug: String,
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration']
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size']
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty'],
-        enum: {
-            values: ['easy', 'medium', 'difficult'],
-            message: 'Difficulty is either easy, medium, difficult'
-        }
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5,
-        min: [1, 'Rating must be above 1.0'],
-        max: [5, 'Rating must be below 5.0']
-    },
-    ratingsQuantity: {
-        type: Number, default: 0
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    },
-    priceDiscount: {
-        type: Number,
-        validate: {
-            validator: function (val) {
-                //this only points to current doc on New document creation
-                return val < this.price;
-            },
-            message: 'Discount price ({VALUE}) should be below regular price'
-        }
-    },
-    summary: {
-        type: String,
-        trim: true
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image']
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false
-    },
-    startDates: [Date],
-    secretTour: {
-        type: Boolean,
-        default: false
-    }
-},{
-    toJSON: { virtuals: true },
-    toObect: { virtuals: true}
-})
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+            trim: true,
+            maxlength: [40, 'A tour name must be less or equal to 40 characters'],
+            minlength: [10, 'A tour name must have more or equal to 10 characters'],
+            // validate: [validator.isAlpha, 'Tour name must only contain characters']
+        },
+        slug: String,
 
-tourSchema.virtual('durationWeeks').get(function() {
-    return this.duration / 7;
-})
+        duration: {
+            type: Number,
+            required: [true, "A must have a duration "]
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A tour must have a group size']
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A tour must have a difficulty'],
+            enum: {
+                values: ['easy', 'medium', 'difficult'],
+                message: 'Difficulty is either: easy, medium, difficult'
+            }
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 4.5,
+            min: [1, 'Rating must be above 1.0'],
+            max: [5, 'Rating must be below 5.0'],
+            set: val => Math.round(val * 10)/10  //4.6666, 46.666,  47, 4.7
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price']
+        },
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    //this only point to current doc on new document creation
+                    return val < this.price;
+                },
+                message: 'Discount price ({VALUE}) should be below regular price'
+            }
+        },
+        summary: {
+            type: String,
+            trim: true,
+            required: [true, 'A tour must have a summary']
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have an image cover']
+        },
+        images: [String],
+
+        startDates: [Date],
+
+        secretTour: {
+            type: Boolean,
+            default: false
+        },
+
+        startLocation: {
+            //GeoJSON
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point']
+            },
+            coordinates: [Number],
+            address: String,
+            description: String
+        },
+        locations: [
+            {
+                type: {
+                    type: String,
+                    default: 'Point',
+                    enum: ['Point']
+                },
+                coordinates: [Number],
+                address: String,
+                description: String,
+                day: Number
+            }
+        ],
+        // guides: [
+        //     {
+        //         type: mongoose.Schema.ObjectId,
+        //         ref: 'User'
+        //     }
+        // ]
+    },
+    {
+        timestamps: true,
+        toJSON: {virtuals: true},
+        toObject: {virtuals: true}
+    });
+
+
 
 // Document Middleware: runs before .save() $ .create()
 tourSchema.pre('save', function(next) {
