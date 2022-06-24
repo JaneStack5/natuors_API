8a01cc50370876a40b255ee3bdb050de60985ecd
JaneStack <mrumunstack@gmail.com>
2022-06-24 13:53:57 -0700

    Tour guides embedding

--- models/Tour.js
+++ models/Tour.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./User')
 
 const tourSchema = new mongoose.Schema({
         name: {
@@ -100,12 +101,12 @@ const tourSchema = new mongoose.Schema({
                 day: Number
             }
         ],
-        // guides: [
-        //     {
-        //         type: mongoose.Schema.ObjectId,
-        //         ref: 'User'
-        //     }
-        // ]
+        guides: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
     {
         timestamps: true,
@@ -120,6 +121,14 @@ tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true});
     next();
 });
+
+//connecting users with tour by embedding
+tourSchema.pre('save', async function (next) {
+    const guidesPromises = this.guides.map(async id => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises)
+
+    next();
+})
 
 // tourSchema.post('save', function(doc, next) {
 //     console.log(doc);
